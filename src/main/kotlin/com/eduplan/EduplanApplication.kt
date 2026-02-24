e7383c05f2c35fb810ecc74158a3f6b23946e905
V9kerNiene <garry_gan@outlook.com>
2026-02-24 18:52:07 +0300

    fix: КОНФИГ РАБОТАЕТ, СЮДААА

--- src/main/kotlin/com/eduplan/EduplanApplication.kt
+++ src/main/kotlin/com/eduplan/EduplanApplication.kt
@@ -1,9 +1,12 @@
 package com.eduplan
 
+import com.eduplan.domain.model.Properties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(Properties::class)
 class EduplanApplication
 
 fun main(args: Array<String>) {
