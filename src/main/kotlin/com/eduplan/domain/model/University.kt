3a6e61b4aeaf930bdfa6b25e7a8ddaa3e111d2db
Mnikii <lysovniki@gmail.com>
2026-03-07 14:05:03 +0300

    feat: implemented base, user, university domain model

--- src/main/kotlin/com/eduplan/domain/model/University.kt
+++ src/main/kotlin/com/eduplan/domain/model/University.kt
@@ -3,9 +3,10 @@ package com.eduplan.domain.model
 import java.util.UUID
 
 data class University(
-    val universityId: UUID,
+    val id: UUID,
     var name: String,
     val address: String,
-    val structure: MutableMap<String, MutableList<String>> // Key - faculty; Value - list of programmes on a faculty
+    val website: String,
+//    val structure: MutableMap<String, MutableList<String>> // Key - faculty; Value - list of programmes on a faculty
 ) {
 }
