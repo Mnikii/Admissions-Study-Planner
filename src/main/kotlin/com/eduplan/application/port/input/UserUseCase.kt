83a40fa4097330652901fbf7cbe76875d99bdc05
Mnikii <lysovniki@gmail.com>
2026-04-14 21:27:53 +0300

    feat: create user usecase (interface for controller)

--- src/main/kotlin/com/eduplan/application/port/input/UserUseCase.kt
+++ src/main/kotlin/com/eduplan/application/port/input/UserUseCase.kt
@@ -1,4 +1,36 @@
 package com.eduplan.application.port.input
 
-class UserUseCase {
+import com.eduplan.domain.model.User
+import java.time.LocalDate
+import java.util.*
+
+interface UserUseCase {
+    fun createUser(command: CreateUserCommand): User
+
+    fun getUserById(id: UUID): User?
+
+    fun updateUser(
+        id: UUID,
+        command: UpdateUserCommand,
+    ): User
+
+    fun deleteUser(id: UUID)
+
+    fun getAllUsers(): List<User>
+
+
+    data class CreateUserCommand(
+        val username: String,
+        val firstName: String,
+        val lastName: String,
+        val email: String,
+        val phoneNumber: String,
+        val birthday: LocalDate,
+    )
+
+    data class UpdateUserCommand(
+        val firstName: String,
+        val lastName: String,
+        val phoneNumber: String,
+    )
 }
