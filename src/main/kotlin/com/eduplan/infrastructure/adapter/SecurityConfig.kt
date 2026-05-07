737557ae7a72c18414d8d5edced9fbbcc22b0367
Mnikii <lysovniki@gmail.com>
2026-05-07 21:56:57 +0300

    feat: add role mode with admin

---
+++ src/main/kotlin/com/eduplan/infrastructure/adapter/SecurityConfig.kt
@@ -0,0 +1,58 @@
+package com.eduplan.infrastructure.adapter
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.config.annotation.web.invoke
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize(HttpMethod.GET, "/api/v1/**", hasAnyRole("ADMIN", "USER"))
+                authorize(HttpMethod.POST, "/api/v1/**", hasRole("ADMIN"))
+                authorize(HttpMethod.PUT, "/api/v1/**", hasRole("ADMIN"))
+                authorize(HttpMethod.DELETE, "/api/v1/**", hasRole("ADMIN"))
+                authorize(anyRequest, authenticated)
+
+            }
+            httpBasic { }
+            csrf {
+                disable()
+            }
+        }
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val admin = User.builder()
+            .username("admin")
+            .password(passwordEncoder().encode("admin123"))
+            .roles("ADMIN")
+            .build()
+
+        val user = User.builder()
+            .username("user")
+            .password(passwordEncoder().encode("user123"))
+            .roles("USER")
+            .build()
+
+        return InMemoryUserDetailsManager(admin, user)
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+}
